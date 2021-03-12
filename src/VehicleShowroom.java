532fb3888cf261a42de73fc9dd01fc2ebb050abb
nabil053 <nabil.cse.53@gmail.com>
2021-03-13 02:14:32 +0600

    Added comments and added feature to check for showroom capacity

--- src/VehicleShowroom.java
+++ src/VehicleShowroom.java
@@ -2,20 +2,32 @@ import java.util.ArrayList;
 
 public class VehicleShowroom {
     private int VisitorCount;
+    private int CarCapacity;
+    private int CarCount;
     private ArrayList<Vehicle> VehicleList;
 
-    VehicleShowroom(){
+    VehicleShowroom(int CarCapacity){
+        this.CarCapacity = CarCapacity;
+        CarCount = 0;
         VisitorCount = 30;
         VehicleList = new ArrayList<Vehicle>();
     }
 
-    public void AddVehicle(Vehicle v){
-        if(v instanceof SportsVehicle){
-            VisitorCount += 20;
+    //Add a vehicle to the showroom, as long as the showroom capacity isn't exceeded
+    public boolean AddVehicle(Vehicle v){
+        boolean added_car = false;
+        if(CarCount < CarCapacity){
+            if(v instanceof SportsVehicle){
+                VisitorCount += 20;
+            }
+            VehicleList.add(v);
+            added_car = true;
+            CarCount++;
         }
-        VehicleList.add(v);
+        return added_car;
     }
 
+    //Remove a car from the showroom if it is present in the showroom
     public boolean RemoveVehicle(int ModelNumber){
         int size = VehicleList.size();
         boolean found_model = false;
@@ -32,10 +44,12 @@ public class VehicleShowroom {
         return found_model;
     }
 
+    //Return the list of vehicles
     public ArrayList<Vehicle> GetVehicles(){
         return VehicleList;
     }
 
+    //Return the number of visitors
     public int getVisitorCount() {
         return VisitorCount;
     }
--- src/VehicleShowroomSystem.java
+++ src/VehicleShowroomSystem.java
@@ -3,64 +3,91 @@ import java.util.Scanner;
 
 public class VehicleShowroomSystem {
     public static void main(String[] args){
-        VehicleShowroom myShowroom = new VehicleShowroom();
+        final int CAR_CAPACITY = 50;
+        VehicleShowroom myShowroom = new VehicleShowroom(CAR_CAPACITY);
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to your vehicle showroom system!");
 
         while (true){
             int option;
+
+            //Select an operation for the showroom system
             do{
                 System.out.println("For each of the following operations, please press the corresponding number");
                 System.out.println("1. Add a vehicle");
                 System.out.println("2. Remove a vehicle");
                 System.out.println("3. View list of vehicles and number of visitors");
                 System.out.println("4. Exit");
-                option = sc.nextInt();
+                option = Integer.parseInt(sc.nextLine());
             }while((option < 1) || (option > 4));
 
             if(option == 4){
                 break;
             }
             else{
+                //Perform any one of the three operations on the showroom system
                 int CarOption, ModelNumber, EnginePower, TireSize;
                 switch (option){
                     case 1:
+                        //Select a vehicle type and enter the common details
                         System.out.println("Select the kind of vehicle: (1) Normal, (2) Sports, (3) Heavy");
-                        CarOption = sc.nextInt();
+                        CarOption = Integer.parseInt(sc.nextLine());
                         System.out.print("Enter the model number: ");
-                        ModelNumber = sc.nextInt();
+                        ModelNumber = Integer.parseInt(sc.nextLine());
                         System.out.print("Enter the engine power: ");
-                        EnginePower = sc.nextInt();
+                        EnginePower = Integer.parseInt(sc.nextLine());
                         System.out.print("Enter the tire size: ");
-                        TireSize = sc.nextInt();
+                        TireSize = Integer.parseInt(sc.nextLine());
                         switch (CarOption){
                             case 1:
+                                //Enter the engine type information for common vehicle and add common vehicle
                                 String EngineType;
                                 System.out.print("Enter the engine type: ");
                                 EngineType = sc.nextLine();
                                 Vehicle v = new Vehicle(ModelNumber, EngineType, EnginePower, TireSize);
-                                myShowroom.AddVehicle(v);
+                                boolean is_added = myShowroom.AddVehicle(v);
+                                if(is_added){
+                                    System.out.println("Car added successfully!");
+                                }
+                                else{
+                                    System.out.println("Maximum showroom capacity reached!");
+                                }
                                 break;
                             case 2:
+                                //Enter the turbo information for sport vehicle and add sport vehicle
                                 int Turbo;
                                 System.out.print("Enter the turbo value: ");
-                                Turbo = sc.nextInt();
+                                Turbo = Integer.parseInt(sc.nextLine());
                                 Vehicle v_sport = new SportsVehicle(ModelNumber, EnginePower, TireSize, Turbo);
-                                myShowroom.AddVehicle(v_sport);
+                                boolean is_added_sport = myShowroom.AddVehicle(v_sport);
+                                if(is_added_sport){
+                                    System.out.println("Car added successfully!");
+                                }
+                                else{
+                                    System.out.println("Maximum showroom capacity reached!");
+                                }
                                 break;
                             case 3:
+                                //Enter the turbo information for heavy vehicle and add heavy vehicle
                                 int Weight;
                                 System.out.print("Enter the weight: ");
-                                Weight = sc.nextInt();
+                                Weight = Integer.parseInt(sc.nextLine());
                                 Vehicle v_heavy = new HeavyVehicle(ModelNumber, EnginePower, TireSize, Weight);
-                                myShowroom.AddVehicle(v_heavy);
+                                boolean is_added_heavy = myShowroom.AddVehicle(v_heavy);
+                                if(is_added_heavy){
+                                    System.out.println("Car added successfully!");
+                                }
+                                else{
+                                    System.out.println("Maximum showroom capacity reached!");
+                                }
                                 break;
                         }
                         break;
                     case 2:
+                        //Enter model number of vehicle and delete vehicle with that model number
                         System.out.print("Enter the vehicle's model number: ");
-                        int ModelNumberRemove = sc.nextInt();
+                        int ModelNumberRemove = Integer.parseInt(sc.nextLine());
                         boolean is_removed = myShowroom.RemoveVehicle(ModelNumberRemove);
                         if(is_removed){
                             System.out.println("Vehicle successfully removed");
@@ -70,9 +97,10 @@ public class VehicleShowroomSystem {
                         }
                         break;
                     case 3:
+                        //Display information on all cars along with expected number of visitors
                         ArrayList<Vehicle> VehicleList = myShowroom.GetVehicles();
-                        for(int i = 1; i <= VehicleList.size(); i++){
-                            System.out.println("Car " + i + ":");
+                        for(int i = 0; i < VehicleList.size(); i++){
+                            System.out.println("Car " + (i + 1) + ":");
                             System.out.println("Model Number: " + VehicleList.get(i).getModelNumber());
                             System.out.println("Engine Type: " + VehicleList.get(i).getEngineType());
                             System.out.println("Engine Power: " + VehicleList.get(i).getEnginePower());
