2db05a5abebe124ae8473ae358a677b6ea6af721
nabil053 <nabil.cse.53@gmail.com>
2021-03-12 22:42:44 +0600

    Created VehicleShowroom class

---
+++ src/VehicleShowroom.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class VehicleShowroom {
+    private int VisitorCount;
+    private ArrayList<Vehicle> VehicleList;
+
+    VehicleShowroom(){
+        VisitorCount = 30;
+        VehicleList = new ArrayList<Vehicle>();
+    }
+
+    public void AddVehicle(Vehicle v){
+        if(v instanceof SportsVehicle){
+            VisitorCount += 20;
+        }
+        VehicleList.add(v);
+    }
+
+    public boolean RemoveVehicle(int ModelNumber){
+        int size = VehicleList.size();
+        boolean found_model = false;
+        for(int i = 0; i < size; i++){
+            if(VehicleList.get(i).getModelNumber() == ModelNumber){
+                VehicleList.remove(i);
+                found_model = true;
+                break;
+            }
+        }
+        return found_model;
+    }
+
+    public ArrayList<Vehicle> GetVehicles(){
+        return VehicleList;
+    }
+}
---
+++ src/VehicleShowroomSystem.java
@@ -0,0 +1,5 @@
+public class VehicleShowroomSystem {
+    public static void main(String[] args){
+
+    }
+}
